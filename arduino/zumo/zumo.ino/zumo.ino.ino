4e688f8a582fa2fb7a8583057377cea96c191769
edvardmb <edvardm@stud.ntnu.no>
2018-03-12 18:44:47 +0100

    Fikset koden fra Bouvet-kurset

--- arduino/zumo/zumo.ino/zumo.ino.ino
+++ arduino/zumo/zumo.ino/zumo.ino.ino
@@ -1,66 +1,50 @@
-#include <Zumo32U4.h>
-#include <Zumo32U4Motors.h>
+#include <ZumoMotors.h>
+#include <Pushbutton.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <QTRSensors.h>
 
-#include <Wire.h>
-#include <Zumo32U4.h>
+ZumoMotors motors;
+ZumoReflectanceSensorArray lineSensors;
+Pushbutton button(ZUMO_BUTTON);
 
 // This might need to be tuned for different lighting conditions,
 // surfaces, etc.
 #define QTR_THRESHOLD     1000  // microseconds
+const int MAX_SPEED = 400;
+int leftMotorSpeed = 100;
+int rightMotorSpeed = 100;
 
 // These might need to be tuned for different motor types.
 #define REVERSE_SPEED     200  // 0 is stopped, 400 is full speed
 #define TURN_SPEED        150
-#define FORWARD_SPEED     400
+#define FORWARD_SPEED     300
 #define REVERSE_DURATION  200  // ms
 #define TURN_DURATION     300  // ms
 
-Zumo32U4LCD lcd;
-Zumo32U4ButtonB buttonB;
-Zumo32U4Buzzer buzzer;
-Zumo32U4Motors motors;
-Zumo32U4LineSensors lineSensors;
 
 
 
-#define NUM_SENSORS 3
+
+#define NUM_SENSORS 6
 unsigned int lineSensorValues[NUM_SENSORS];
 
 void waitForButtonAndCountDown()
 {
-  ledYellow(1);
-  lcd.clear();
-  lcd.print(F("Press B"));
-
-  buttonB.waitForButton();
-
-  ledYellow(0);
-  lcd.clear();
-  
-  for (int i = 0; i < 3; i++)
-  {
-    delay(1000);
-    buzzer.playNote(NOTE_G(3), 200, 15);
-  }
-  delay(1000);
-  buzzer.playNote(NOTE_G(1), 500, 15);
-  delay(1000);
+  button.waitForButton();
 }
 
 void setup() {
-  // put your setup code here, to run once:
-   lcd.print(F("Press B"));
    waitForButtonAndCountDown();
-   lineSensors.initThreeSensors();
+   lineSensors.init();
 }
 
 void loop() {
-   if (buttonB.isPressed())
+   if (button.isPressed())
   {
     // If button is pressed, stop and wait for another press to
     // go again.
     motors.setSpeeds(0, 0);
-    buttonB.waitForRelease();
+    button.waitForRelease();
     waitForButtonAndCountDown();
   }
 
@@ -86,8 +70,10 @@ void loop() {
   {
     // Otherwise, go straight.
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-    buzzer.playFrequency(6000,200,15);
   }
 }
+
+
+
 
 
