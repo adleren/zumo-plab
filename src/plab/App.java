b65d2c35c008e8e5df290976da1571840b9dc378
Adrian Leren <adrian.leren@hotmail.com>
2018-03-21 14:19:06 +0100

    Added forced termination when window is closed in UI

--- src/plab/App.java
+++ src/plab/App.java
@@ -6,10 +6,13 @@
 package plab;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class App extends Application {
 
@@ -19,6 +22,15 @@ public class App extends Application {
         Scene scene = new Scene(root, 600, 400);
         stage.setTitle("Zumo UI");
         stage.setScene(scene);
+        
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+        
         stage.show();
 	}
 
